67f9e44d86bb6e7bf37ca79c541cc036194fc430
Ela Korkmaz <elakorkmaz87@gmail.com>
2016-10-30 16:48:44 +0100

    search is working

--- index.js
+++ index.js
@@ -11,10 +11,12 @@ app.use(morgan('dev'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(express.static(__dirname + '/public'));
+
 app.set('view engine', 'pug');
 
 app.get('/', (request, response) => {
-  response.render('index', { users: userStore });
+  response.render('users/index', { users: userStore });
 });
 
 app.get('/search', (request, response) => {
@@ -26,15 +28,51 @@ app.post('/search', (request, response) => {
   response.redirect('/search/' + request.body.query);
 });
 
-app.get('/search/*', (request, response) => {
-  console.log(request.params[0]);
-  response.send('search a user with the query: ' + request.params[0]);
+app.get('/search/*', (req, res) => {
+  var results = searchUsers(req.params[0]);
+  res.render('search-result.pug', { results: results });
+});
 
-  // make the actual search here!!:
+app.get('/form', (request, response) => {
+  response.render('form.pug');
+});
 
-  // if there is a found user we can render a template;
+app.post('/form', (request, response) => {
+  userStore.push(request.body);
+
+  response.redirect('/');
+
+  fs.writeFile('users.json', JSON.stringify(userStore), (error, data) => {
+    if (error) {
+      throw error;
+    }
+  });
+});
+
+app.get('/form', (req, res) => {
+  res.render('form.pug');
 });
 
 app.listen(3000, () => {
   console.log('Web Server is running on port 3000');
 });
+
+function searchUsers(input) {
+  var results = [];
+
+  for (var i = 0; i < userStore.length; i++) {
+    if (searchFirstName(input, userStore[i]) || searchLastName(input, userStore[i])) {
+      results.push(userStore[i]);
+    }
+  }
+
+  return results;
+}
+
+function searchFirstName(input, user) {
+  return user.firstname.toLowerCase().includes(input.toLowerCase());
+}
+
+function searchLastName(input, user) {
+  return user.lastname.toLowerCase().includes(input.toLowerCase());
+}
